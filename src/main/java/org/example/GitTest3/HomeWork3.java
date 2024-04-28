d1723608a778a1e7e9e937fe6afa5d8ba69b122b
kinomagnum <kondratqa@yandex.ru>
2024-04-29 00:23:19 +0300

    Vetka4cod_DZ4_1

--- src/main/java/org/example/GitTest3/HomeWork3.java
+++ src/main/java/org/example/GitTest3/HomeWork3.java
@@ -2,6 +2,6 @@ package org.example.GitTest3;
 
 public class HomeWork3 {
     public static void main(String[] args) {
-        System.out.println("DZ4!!!");
+        System.out.println("DZ4!!!!!!");
     }
 }
